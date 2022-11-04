f9672118e4096091bab7acf1ddcd93cb51d719d2
Simon Garner <simon@equalogic.com>
2022-11-04 15:45:44 +1300

    Rename GraphQLInspection.isSelected() -> has()

--- src/GraphQLInspection.ts
+++ src/GraphQLInspection.ts
@@ -1,22 +1,14 @@
 import { GraphQLResolveInfo, SelectionNode } from 'graphql';
-import { findSelectionNode } from './functions';
+import { findSelectionNode, isFieldSelected } from './functions';
 
 export class GraphQLInspection {
   public constructor(public readonly info: GraphQLResolveInfo) {}
 
-  /**
-   * Returns the SelectionNode for referenced field if it was selected in the GraphQL query, or null if it's not found.
-   * `fieldPath` can reference nested fields using dotted 'parentField.childField.grandchildField' notation.
-   */
   public find(fieldPath: string): SelectionNode | null {
     return findSelectionNode(fieldPath, this.info);
   }
 
-  /**
-   * Returns true if the referenced field was selected in the GraphQL query, or false if it's not found.
-   * `fieldPath` can reference nested fields using dotted 'parentField.childField.grandchildField' notation.
-   */
-  public isSelected(fieldPath: string): boolean {
-    return this.find(fieldPath) != null;
+  public has(fieldPath: string): boolean {
+    return isFieldSelected(fieldPath, this.info);
   }
 }
